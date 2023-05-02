368000508fa5c7c083f2bf6483c31ab8576d847e
ArbidonaH <ah52242@ubt-uni.net>
2023-05-02 21:19:20 +0200

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,22 @@ import {Container} from 'react-bootstrap';
 import Header from  './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import {BrowserRouter as Router, Routes } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen';
 
 const App=()=> {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen/>
+          <Routes path='/' component={HomeScreen} exact/>
+          <Routes path='/product/:id' component={ProductScreen} exact/>
         </Container>
       </main>
       <Footer />
-    </>
+      </Router>
+   
   );
 }
 
