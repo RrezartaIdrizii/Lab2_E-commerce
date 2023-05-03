2cad031c264e758e31fffdb054522d7a6df4e5a1
RrezartaIdrizii <104738075+RrezartaIdrizii@users.noreply.github.com>
2023-05-04 01:58:03 +0200

    Connecting the database

---
+++ backend/data/users.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true
+    },
+    {
+        name: 'Rrezarta Idrizi',
+        email: 'rrezarta@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Arbidona Hodaj',
+        email: 'arbidona@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    },
+    {
+        name: 'Enis Hamza',
+        email: 'enis@example.com',
+        password: bcrypt.hashSync('123456', 10),
+    }
+]
+
+export default users
